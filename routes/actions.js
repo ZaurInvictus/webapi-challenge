7b88264ba33fc05af4fea41ca7dfce6bbce885c6
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-17 12:53:33 -0500

    POST handler for actions created

--- routes/actions.js
+++ routes/actions.js
@@ -19,4 +19,60 @@ router.get('/', (req, res) => {
 })
 
 
+// POST ACTION
+router.post("/:id", validateProjectId, (req, res) => {
+  const { id }  = req.params
+  const { description, notes } = req.body
+
+  if(!notes || !description) {
+    res.status(400).json({
+      message: "Please provide notes and description!"
+    })
+    return
+  }
+
+  if(!req.project) {
+    res.status(400).json({
+      message: "Invalid project ID!"
+    })
+    return
+  }
+
+  Actions.insert({description, notes, project_id: id})
+  .then(action => {
+      res.status(201).json(action)
+  })
+   .catch(() => {
+     res.status(500).json({
+       error: 'Internal server error'
+     })
+   })
+})
+
+
+
+// CUSTOM MIDDLEWARE
+
+// VALIDATE PROJECT ID
+function validateProjectId(req, res, next) {
+  
+  Projects.get(req.params.id)
+
+  .then(project => {
+    if(project) {
+      req.project = project // we are storing project on req object
+      next()
+    } else {
+      res.status(400).json({
+        message: "invalid project id" 
+      })
+    }
+  })
+  .catch(() => {
+    res.status(500).json({
+      error: 'Internal server error'
+    })
+  })
+};
+
 module.exports = router
